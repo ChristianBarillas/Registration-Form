f109eb055f6eef09ae3ed6303b526bc120823c50
christianbarillas <christianbarillasyt@gmail.com>
2022-12-19 11:24:56 -0600

    Registration Form 0.1

---
+++ capa.js
@@ -0,0 +1,66 @@
+window.onload = function (){
+
+
+
+
+const input = document.getElementById ('inputcaja');
+
+//input.style = 'color: #0474BC';
+input.style.color ='yellow'
+
+const titulo1 = document.querySelector('.titulo1');
+
+titulo1.style = 'background-color: #0474BC';
+
+const titulo2 = document.querySelector('.titulo2');
+
+titulo2.style = 'background-color:#0474BC';
+
+
+
+}
+
+//const nombre = document.getElementById("inputcaja");
+// nombre.setAttribute('title','este es un texto xddd')
+
+// para acceder directo al aatributo usando javascript sin usar metodos previamente visto
+
+
+/*const input = document.getElementById('inputcaja');
+
+input.minLength = '4';*/
+
+//titulo2.style.backgroundColor = 'blue'
+
+/*
+
+let valor = titulo2.classList.contains('pajaros');
+
+if (valor){
+
+document.write('la clase si existe');
+
+}
+
+else
+{
+    document.write('la clase no exsiteee');
+} 
+*/
+/*
+
+classlist metodos
+
+classlist.add
+classlist.remove
+classlist.item
+classlist.contains
+classlist.toogle (lo mismo de la siguiente pero devuelve true y remplaza automaticamente, devuelve tue o false)
+classlist.replace ('valor que eiste', 'valor al que se remplaz') devulve true
+
+
+*/
+
+
+
+
